9c24ecffa62fdcbb74700fee5794add47347ce13
BenMradB <bilelbenmrad2001@gmail.com>
2022-09-03 11:46:52 +0100

    Update Inspire Project Code

---
+++ controllers/Uploading/uploadVideo.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const db = require('../../config/db');
+const path = require('path');
+const moment = require('moment');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: `./public/images/videoURLs`,
+    filename: (req, file, cb) => {
+        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({
+    storage,
+}).single('video_URL');
+
+const addVideo = (req, res) => {
+    upload(req, res, (err) => {
+        if (err) return res.redirect(`/edit-course/${req.params.id}?error=${encodeURIComponent(err)}`);
+
+        if (!req.file) return res.redirect(`/edit-course/${req.params.id}?error=${encodeURIComponent('Choose The Vide URL  ')}`);
+
+        // extract the file extention from the uploaded file
+        const fext = path.extname(req.file.originalname);
+
+        if ( fext !== '.mp4' && fext !== '.MP4' ) return res.redirect(`/edit-course/${req.params.id}?error=${encodeURIComponent('accepted extention : .mp4 OR .MP4')}`);
+        let sql = `INSERT INTO videos SET ?`;
+
+        db.query(sql, { course_id: req.params.id, trainer_id: req.user.id_formateur, creation: moment().format('YYYY-MM-D'), video_URL: req.file.filename }, (err, row) => {
+            if (err) throw err;
+
+            let sql = `UPDATE formation SET  nb_videos = nb_videos + 1  WHERE id_form = ${req.params.id}`;
+
+            db.query(sql, (err, rows) => {
+                if (err) throw err;
+
+                return res.redirect(`/edit-course/${req.params.id}?success=${encodeURIComponent('Vido Added To This Course successfully')}`);
+
+            });
+        });
+    });
+}
+
+module.exports = {addVideo};
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,29 @@
 const { Router } = require('express');
 const router = Router();
-const { getHomePage, getLoginPage, getSignupPage, getDashboardPage, getEditProfilePage, getProfilePage, getMessagesPage, getTrainerMessagesPage, getChatWithSomeOnePage, getTrainerLoginPage, getTrainerDashboardPage, getEditTrainerProfilePage, getChatWithAStudentPage, getCreateCoursePage, getTrainerCourses, getEditCoursePage, getByCoursesPage, getStudentCoursesPage, logout, login, signup, editAvatar, editProfile, editPassword, editTrainerPassword, storePrivateMessages, trainerLogin, editTrainerProfile, editTrainerAvatar, storePrivateTrainerMessages, createCourse, byACourse } = require('../controllers/auth');
-const { addVideo } = require('../controllers/uploadVideo');
-const { editCourseThubmnail } = require('../controllers/uploadCourseThubmnails');
+
+// Global Functions
+const { getHomePage, logout } = require('../controllers/auth');
+
+// User Functions 
+const { getLoginPage, getSignupPage, getDashboardPage, getEditProfilePage, getMessagesPage, getChatWithSomeOnePage, getStudentCoursesPage, getByCoursesPage, login, signup, editProfile, editPassword, storePrivateMessages, byACourse} = require('../controllers/User/user')
+
+// Teacher Functions
+const { getTrainerDashboardPage,  getTrainerMessagesPage, getTrainerLoginPage, getEditTrainerProfilePage, getChatWithAStudentPage, getCreateCoursePage, getTrainerCourses, getEditCoursePage, editTrainerProfile, trainerLogin, editTrainerPassword, storePrivateTrainerMessages, createCourse } = require('../controllers/Teacher/teacher');
+
+// Upload Files Functions
+const { editUserAvatar, editTrainerAvatar } = require('../controllers/Uploading/profiles');
+const { addVideo } = require('../controllers/Uploading/uploadVideo');
+const { editCourseThubmnail } = require('../controllers/Uploading/uploadCourseThubmnails');
+
+// Middelwares
 const loggedin = require('../middlewares/loggedin');
 const loggedout = require('../middlewares/loggedout');
 const trainerLoggedin = require('../middlewares/trainerLoggedin');
+
+// Function For Validation Form Fileds
 const validation = require('../middlewares/userValidationMiddleware');
+
+// Scheamas To Every Form
 const loginSchema = require('../validation/loginValidation');
 const signupSchema = require('../validation/signupValidations');
 const editProfileSchema = require('../validation/editProfileValidation');
@@ -15,40 +32,37 @@ const chatSchema = require('../validation/chatValidation');
 const trainereditProfileSchema = require('../validation/trainereditProfileValidation');
 const createCourseSchema = require('../validation/createCourseValidation');
 
+// Global Request (Used By The User And The Teacher)
 router.get('/', getHomePage);
+
+// Start User Requests Section 
 router.get('/dashboard', loggedin, getDashboardPage);
-// router.get('/profile', loggedin, getProfilePage);
 router.get('/logout', loggedin, logout);
-
-router.get('/trainer-dashboard', trainerLoggedin, getTrainerDashboardPage);
-router.post('/profile/edit-trainer-avatar/:id', editTrainerAvatar);
-router.route('/trainer-login').get(loggedout, getTrainerLoginPage).post(validation(loginSchema), trainerLogin);
-router.route('/trainer-contact').get(trainerLoggedin, getTrainerMessagesPage);
-router.route('/profile/edit-trainer-profile/:id').get(trainerLoggedin, getEditTrainerProfilePage).post(validation(trainereditProfileSchema), editTrainerProfile);
-router.post('/profile/edit-trainer-password/:id', validation(editPasswordSchema), editTrainerPassword);
-
-router.post('/profile/edit-avatar/:id', editAvatar);
+router.get('/by-courses', loggedin, getByCoursesPage);
+router.get('/student-courses', loggedin, getStudentCoursesPage);
+router.post('/profile/edit-avatar/:id', editUserAvatar);
 router.post('/profile/edit-password/:id', validation(editPasswordSchema), editPassword);
-
+router.post('/by-course/:id', loggedin, byACourse);
 router.route('/login').get(loggedout, getLoginPage).post(validation(loginSchema), login);
 router.route('/signup').get(loggedout, getSignupPage).post(validation(signupSchema), signup);
 router.route('/profile/edit-profile/:id').get(loggedin, getEditProfilePage).post(validation(editProfileSchema), editProfile);
 router.route('/start-chating').get(loggedin, getMessagesPage);
-
 router.route('/start-chating/with/:id').get(loggedin, getChatWithSomeOnePage).post(loggedin, validation(chatSchema), storePrivateMessages);
-router.route('/start-chating/with-student/:id').get(trainerLoggedin, getChatWithAStudentPage).post(trainerLoggedin, validation(chatSchema), storePrivateTrainerMessages);
+// End User Section
 
-// router.post('/delete-trainer-conversation/:id', deleteTrainerConversation);
-
-router.route('/create-course/:id').get(trainerLoggedin, getCreateCoursePage).post(validation(createCourseSchema), createCourse);
+// Start Teacher Requests Section 
+router.get('/trainer-dashboard', trainerLoggedin, getTrainerDashboardPage);
 router.get('/trainer-courses', trainerLoggedin, getTrainerCourses);
 router.get('/edit-course/:id', trainerLoggedin, getEditCoursePage);
 router.post('/edit-course-thubmnail/:id', editCourseThubmnail);
 router.post('/add-video/:id', trainerLoggedin, addVideo);
-
-router.get('/by-courses', loggedin, getByCoursesPage);
-router.post('/by-course/:id', loggedin, byACourse);
-
-router.get('/student-courses', loggedin, getStudentCoursesPage);
+router.post('/profile/edit-trainer-avatar/:id', editTrainerAvatar);
+router.post('/profile/edit-trainer-password/:id', validation(editPasswordSchema), editTrainerPassword);
+router.route('/trainer-login').get(loggedout, getTrainerLoginPage).post(validation(loginSchema), trainerLogin);
+router.route('/trainer-contact').get(trainerLoggedin, getTrainerMessagesPage);
+router.route('/profile/edit-trainer-profile/:id').get(trainerLoggedin, getEditTrainerProfilePage).post(validation(trainereditProfileSchema), editTrainerProfile);
+router.route('/start-chating/with-student/:id').get(trainerLoggedin, getChatWithAStudentPage).post(trainerLoggedin, validation(chatSchema), storePrivateTrainerMessages);
+router.route('/create-course/:id').get(trainerLoggedin, getCreateCoursePage).post(validation(createCourseSchema), createCourse);
+// End User Section
 
 module.exports = router;
